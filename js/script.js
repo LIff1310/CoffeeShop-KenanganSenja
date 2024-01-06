e1cfb848ba6ba74ecb889277b248c1128e038511
Alif <firjatullahalif1310@gmail.com>
2024-01-06 11:24:54 +0700

    git committed

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
-// Togle Class Active
+// Togle Class Active Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika hamburger diklik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik diluar sidebar untuk keluar
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle Class Active Search Form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
+
+// klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector('#search-button');
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
